c069fabaa15a971f2714252fa390664209fd99ee
trofimovama <98819201+trofimovama@users.noreply.github.com>
2023-06-12 09:24:52 +0200

    [#8] Introduce set of todo-lists (PR #9)

--- script.js
+++ script.js
@@ -1,53 +1,105 @@
-const inputField = document.getElementById('input-field');
-const listContainer = document.getElementById('list-container');
+const container = document.getElementById("container");
+const id = Date. now();
+
+const addToListButton = document.getElementById('add-lists');
 const deleteItemTag = 'span';
 const inputItemTag = 'input';
 
-const storeData = () => {
-  localStorage.setItem("data", listContainer.innerHTML);
+const saveData = () => {
+  localStorage.setItem('data', container.innerHTML);
 };
 
 const displayData = () => {
-  listContainer.innerHTML = localStorage.getItem("data");
+  container.innerHTML = localStorage.getItem("data");
 };
 
-const addItemToList = () => {
-  const listItem = document.createElement('li');
-  const listItemInput = document.createElement(inputItemTag);
-  listItemInput.setAttribute("type", "checkbox");
-  const listItemLabel = document.createElement('label');
+const addCardContainer = () => {
 
-  listItemLabel.innerHTML = inputField.value;
+  const addListItem = () => {
+    const listItem = document.createElement('li');
+  
+      const listItemInput = document.createElement(inputItemTag);
+      listItemInput.setAttribute("type", "checkbox");
 
-  listContainer.appendChild(listItem);
-  listItem.appendChild(listItemInput);
-  listItem.appendChild(listItemLabel);
+      const listItemLabel = document.createElement('label');
+      listItemLabel.innerHTML = inputField.value;
 
-  const deleteItem = document.createElement(deleteItemTag);
-  deleteItem.innerHTML = "☒";
-  listItem.appendChild(deleteItem);
+      const deleteItem = document.createElement(deleteItemTag);
+      deleteItem.innerHTML = "☒";
 
-  inputField.value = "";
-  storeData();
-};
+      listContainer.appendChild(listItem);
 
-listContainer.addEventListener("click", (e) => {
-  if(e.target.tagName.toLowerCase() === deleteItemTag) {
-    e.target.parentElement.remove();
-  } else if (e.target.tagName.toLowerCase() === inputItemTag) {
-    e.target.toggleAttribute('checked');
-  }
-  storeData();
-});
+      listItem.append(listItemInput, listItemLabel, deleteItem);
 
-inputField.addEventListener("keydown", (e) => {
-  if (inputField.value) {
-    if (e.key === "Enter") {
-      addItemToList();
-    } else if (e.key === "Escape") {
       inputField.value = "";
-    }
+      saveData();
   };
+
+  const cardContainer = document.createElement('div');
+  cardContainer.setAttribute("class", "list-app");
+
+  const deleteCardIcon = document.createElement('p');
+  deleteCardIcon.innerHTML = "✖";
+  deleteCardIcon.setAttribute("class", "delete-list-icon");
+  deleteCardIcon.addEventListener("click", (e) => {
+    if (e.target.tagName.toLowerCase() === "p") {
+      e.target.parentElement.remove();
+      saveData();
+    };
+  });
+
+  const cardTitle = document.createElement('h1');
+  cardTitle.setAttribute("class", "list-title");
+  cardTitle.innerHTML = "Plans";
+
+  const inputContainer = document.createElement('div');
+  inputContainer.setAttribute("class", "list-item");
+
+  const inputField = document.createElement('input');
+  inputField.setAttribute("type", "text");
+  inputField.setAttribute("id", id);
+  inputField.setAttribute("placeholder", "Add something...");
+  inputField.addEventListener("keydown", (e) => {
+    if (inputField.value) {
+      if (e.key === "Enter") {
+        addListItem();
+      } else if (e.key === "Escape") {
+        inputField.value = "";
+      }
+      saveData();
+    };
+  });
+
+  const listButton = document.createElement('button');
+  listButton.setAttribute("class", "button");
+  listButton.innerHTML = "Add";
+  listButton.addEventListener("click", () => {
+    if (inputField.value) {
+      addListItem();
+    };
+  });
+
+  const listContainer = document.createElement('ul');
+  listContainer.setAttribute("class", "list-container");
+
+  container.appendChild(cardContainer);
+
+  cardContainer.append(deleteCardIcon, cardTitle, inputContainer, listContainer);
+  
+  inputContainer.append(inputField, listButton);
+
+  listContainer.addEventListener("click", (e) => {
+    if (e.target.tagName.toLowerCase() === deleteItemTag) {
+      e.target.parentElement.remove();
+    } else if (e.target.tagName.toLowerCase() === inputItemTag) {
+      e.target.toggleAttribute('checked');
+    };
+    saveData();
+  });
+};
+
+addToListButton.addEventListener ("click", () => {
+  addCardContainer();
 });
 
 displayData();
