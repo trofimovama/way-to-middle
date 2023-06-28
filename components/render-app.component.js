0d8913db13651b3cf622e234b26d5023804da4be
trofimovama <98819201+trofimovama@users.noreply.github.com>
2023-06-28 07:47:42 +0200

    [#14] Code splitting into components (PR #15)

---
+++ components/render-app.component.js
@@ -0,0 +1,43 @@
+import { renderTodoList } from './render-todo-list.component.js';
+import { rootState, notifyStateUpdated } from '../state/root.state.js';
+
+export const rootContainer = document.getElementById("container");
+
+export const renderApp = (appState) => {
+  const addListsButton = document.getElementById("add-lists");
+
+  const todoListElems = appState
+  .map((todoListData) => {
+    const destroyFromState = () => {
+      const i = appState.indexOf(todoListData);
+      appState.splice(i, 1);
+
+      notifyStateUpdated();
+    };
+
+    const component = renderTodoList(todoListData, destroyFromState);
+
+    return component;
+  })
+  .map(({ elem }) => elem);
+  rootContainer.append(...todoListElems);
+
+  addListsButton.addEventListener('click', () => {
+    const newListData = [];
+    rootState.push(newListData);
+
+    const { elem } = renderTodoList(newListData, () => {
+      const i = appState.indexOf(newListData);
+      appState.splice(i, 1);
+
+      notifyStateUpdated();
+    });
+    rootContainer.append(elem);
+
+    notifyStateUpdated();
+  });
+
+  return {
+    elem: rootContainer
+  };
+};
